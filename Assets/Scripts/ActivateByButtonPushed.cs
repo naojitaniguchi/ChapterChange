ccd93431d2b08ce377d10257e54cfdaad7f0d4be
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-12-16 11:16:45 +0900

    First commit.

---
+++ Assets/Scripts/ActivateByButtonPushed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateByButtonPushed : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void buttonPushed()
+    {
+        target.SetActive(true);
+    }
+}
